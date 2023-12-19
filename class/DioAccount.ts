ef1a2a94b13d1c5adfb06e4ae78e08741cbf3eeb
Evilis Glenio <glenio.developer@gmail.com>
2023-12-19 10:49:10 -0300

    Aqui eu implementei os métodos deposit e saque, conforme o desafio.

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,42 +1,52 @@
 export abstract class DioAccount {
-  private name: string
-  private readonly accountNumber: number
-  balance: number = 0
-  private status: boolean = true
+  private name: string;
+  private readonly accountNumber: number;
+  balance: number = 0;
+  private status: boolean = true;
 
-  constructor(name: string, accountNumber: number){
-    this.name = name
-    this.accountNumber = accountNumber
+  constructor(name: string, accountNumber: number) {
+    this.name = name;
+    this.accountNumber = accountNumber;
   }
 
   setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
+    this.name = name;
+    console.log("Nome alterado com sucesso!");
+  };
 
   getName = (): string => {
-    return this.name
-  }
+    return this.name;
+  };
 
-  deposit = (): void => {
-    if(this.validateStatus()){
-      console.log('Voce depositou')
+  deposit = (newBalance: number): void => {
+    if (this.validateStatus()) {
+      this.balance += newBalance;
     }
-  }
+  };
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
-  }
+  withdraw = (newWithdraw: number): void => {
+    if (this.validateBalance(newWithdraw)) {
+      this.balance -= newWithdraw;
+    }
+  };
 
   getBalance = (): void => {
-    console.log(this.balance)
-  }
+    console.log(this.balance);
+  };
 
   private validateStatus = (): boolean => {
     if (this.status) {
-      return this.status
+      return this.status;
     }
 
-    throw new Error('Conta inválida')
-  }
+    throw new Error("Conta inválida");
+  };
+
+  private validateBalance = (withdraw: number): boolean => {
+    if (this.balance - withdraw >= 0) {
+      return true;
+    }
+
+    throw new Error("Saldo incompatível");
+  };
 }

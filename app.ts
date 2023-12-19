a5f749e2c0601da0108fedde874d2bc8e99db888
Evilis Glenio <glenio.developer@gmail.com>
2023-12-19 10:49:51 -0300

    Incluindo os dados e chamando os métodos.

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from "./class/CompanyAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+// User
+const peopleAccount: PeopleAccount = new PeopleAccount(
+  3136327,
+  "Glenio",
+  20231201
+);
+peopleAccount.deposit(80);
+peopleAccount.withdraw(40);
+console.log(peopleAccount.getBalance());
+
+// Company
+const companyAccount: CompanyAccount = new CompanyAccount("DIO", 20231202);
+companyAccount.deposit(80);
+companyAccount.withdraw(20);
+console.log(companyAccount.getBalance());
